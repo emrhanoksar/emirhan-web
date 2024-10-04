29a3786efb51ce62ab1773b6d8b4029e7191b12b
emrhanoksar <oksar159@hotmail.com>
2024-10-04 19:57:31 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+const githubLink = document.getElementById('github-link');
+const youtubeLink = document.getElementById('youtube-link');
+const instagramLink = document.getElementById('instagram-link');
+
+
+githubLink.addEventListener('click', function() {
+    alert('GitHub profilinize yönlendiriliyorsunuz!');
+});
+
+youtubeLink.addEventListener('click', function() {
+    alert('YouTube kanalınıza yönlendiriliyorsunuz!');
+});
+
+instagramLink.addEventListener('click', function() {
+    alert('Instagram profilinize yönlendiriliyorsunuz!');
+});
